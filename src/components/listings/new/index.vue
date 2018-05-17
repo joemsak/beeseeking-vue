c737053638aa5b8e8010014c5aa694fb4c181446
Joe Sak <joe@joesak.com>
2018-05-17 08:24:04 -0500

    Show details of new listing on side

--- src/components/listings/new/index.vue
+++ src/components/listings/new/index.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="with-backpage">
-    <router-link :to="{ name: 'listings' }" tag="nav" id="backpage">
-      <span>Listings</span>
-    </router-link>
+  <div class="row">
+    <div class="col-sm-10">
+      <div class="with-backpage">
+        <router-link :to="{ name: 'listings' }" tag="nav" id="backpage">
+          <span>Listings</span>
+        </router-link>
 
-    <div class="container new-listing">
-      <h1>List your space</h1>
+        <div class="container new-listing">
+          <h1>List your space</h1>
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
 
-      <router-view></router-view>
+    <div class="col-sm-2">
+      <div class="container new-listing__details">
+        <dl>
+          <dt>Property:</dt>
+          <dd>{{ newListing.propertyType }}</dd>
+
+          <dt>Privacy:</dt>
+          <dd>{{ newListing.privacyType }}</dd>
+
+          <dt>Access:</dt>
+          <dd>{{ newListing.accessType }}</dd>
+
+          <dt>Place:</dt>
+          <dd>{{ newListing.place }}</dd>
+
+          <dt># of Hives:</dt>
+          <dd>{{ newListing.numberOfHives }}</dd>
+
+          <dt>Gateway:</dt>
+          <dd>{{ newListing.gatewayType }}</dd>
+
+          <dt>Hours:</dt>
+          <dd>{{ newListing.hoursAccessible }}</dd>
+        </dl>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'ListYourSpace',
 
@@ -21,12 +53,15 @@ export default {
       place: '',
     }
   },
+
+  computed: mapState(['newListing']),
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-  .new-listing {
+  .new-listing,
+  .new-listing__details {
     padding: 1rem;
   }
 
